60787e085952d239f77a1187daf3c9e06d5a26e6
Phillip Reyes <psreyes@sundevils.asu.edu>
2018-09-10 20:08:12 -0700

    Update
    classes

---
+++ SingletonConsoleApp/Classes/Singleton_Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Classes
+{
+   public sealed class  Singleton_Class
+    {
+        private int incr;
+        private string str;
+        private static Singleton_Class single;
+        private Singleton_Class()
+        {
+            
+
+        }
+        public static Singleton_Class GetReference()
+        {
+            if(single == null)
+            {
+                single = new Singleton_Class();
+                Console.WriteLine("intaintiate object");
+            }
+
+            return single; 
+        }
+        public void printSingleton()
+        {
+            Console.WriteLine("Inside Singleton");
+        }
+
+    }
+     
+}
---
+++ SingletonConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApplication1.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton_Class.GetReference().printSingleton();
+            Singleton_Class.GetReference().printSingleton();
+            Singleton_Class.GetReference().printSingleton();
+            Singleton_Class.GetReference().printSingleton();
+        }
+    }
+}
